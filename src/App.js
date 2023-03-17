7eb42fe9f6654d65ac99ea330db21d0f9615956e
ANIRBAN DUTTA <anirbandutta011@gmail.com>
2023-03-17 20:19:55 +0530

    v8

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>hello from k8s v7</p>
+        <p>hello from k8s v8</p>
         <a
           className="App-link"
           href="https://reactjs.org"
